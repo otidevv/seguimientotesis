a76868b4e12b569b70e9d9feebf32953fac464e6
Alberto <apenam@unamad.edu.pe>
2026-02-10 14:58:33 -0500

    cambio en la ruta de ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'seguimientotesis',
-      script: 'node_modules/.bin/next',
+      script: 'node_modules/next/dist/bin/next',
       args: 'start',
       cwd: './',
       instances: 1,
